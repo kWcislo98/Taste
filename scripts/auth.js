cacaf45a30dff1d2190afef56a3aea178d181d0e
kWcislo98 <47746730+kWcislo98@users.noreply.github.com>
2020-04-18 16:36:43 +0200

    Add files via upload

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,18 +1,77 @@
-//sgnup
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+// listen for auth changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    //get data
+    db.collection("recipes").onSnapshot((snapshot) => {
+      setupRecipes(snapshot.docs);
+      setupUI(user);
+    });
+  } else {
+    setupUI();
+    setupRecipes([]);
+  }
+});
 
-    //get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-
-    //sign up user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-        signupForm.reset();
+// create new recipe
+const createForm = document.querySelector("#create-form");
+createForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("recipes")
+    .add({
+      title: createForm["title"].value,
+      steps: createForm["content"].value,
     })
-})
+    .then(() => {
+      const modal = document.querySelector("#modal-create");
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    })
+    .catch((err) => {
+      window.alert(err.message);
+    });
+});
+//sgnup
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-//logout 
+  //get user info
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+
+  //sign up user
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((cred) => {
+      return db.collection("users").doc(cred.user.uid).set({
+        bio: signupForm["signup-bio"].value,
+      });
+    })
+    .then(() => {
+      const modal = document.querySelector("#modal-signup");
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+    });
+});
+
+//logout
+
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+// login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
